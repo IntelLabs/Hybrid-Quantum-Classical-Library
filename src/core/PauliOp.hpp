db3e7a009e586157d45f23980bb9cc20e49619ba
Atul Kulkarni <atul.kulkarni@intel.com>
2021-08-17 00:41:28 -0500

    maint: Initial building of GoogleTest & PauliOp Class

---
+++ src/core/PauliOp.hpp
@@ -0,0 +1,80 @@
+#include <complex>
+#include <map>
+#include <set>     // These are ordered
+#include <string>
+#include <utility> // pair
+#include <vector>
+
+using op_pair = std::pair<int, char>;
+using pstring = std::set<op_pair>;
+using ComplexDP = std::complex<double>;
+using MapPString = std::map<pstring, ComplexDP>;
+
+class PauliOp {
+
+    /*
+     *
+     * To compile: g++ -std=c++0x PauliOp_Test.cpp
+     *
+     * Eventually need:
+     * -- Commutation graph
+     * -- Qubit-wise commutation graph
+     *
+
+        Methods to add:
+        - equals (assignment)
+        - equality (comparison)
+        - Qubit-wise commutation (QWC) Graph
+        - Commutation graph
+        - Anti-commutation graph
+
+        External methods to add:
+        -
+
+        Done:
+        - addition
+        - multiplication
+        - string-based input
+
+    to do:
+    - get the sting addTerm to work. (do *not* do multi-term)
+    - get scalar mult to work
+    - do assertions
+    - do the other tests above
+
+
+     */
+
+    public:
+        void addTerm(pstring& inpp, ComplexDP k = 1, bool check_validity=true);
+        void addTerm(std::vector<std::string>& vecstr, ComplexDP k = 1);
+        void addIdentTerm(ComplexDP k = 1);
+        bool qubitwiseCommutation(std::vector<std::string>& pauliStrings);
+        op_pair processLocCharString( std::string inp );
+        std::string getCharString();
+
+        PauliOp() = default;
+        PauliOp(const PauliOp&) = default;
+        PauliOp& operator=(const PauliOp& P) { this->op_sum = P.op_sum; return *this; }
+        PauliOp operator+(const PauliOp& inp);
+        PauliOp operator*(const PauliOp& p_right);
+        PauliOp operator*(const ComplexDP& inp_right);
+        bool operator== (const PauliOp& rhs);
+
+        double zero_thresh=1.0e-11;
+
+        MapPString op_sum;  // The data structure
+
+}; // end of class PauliOp
+
+
+
+
+
+
+
+
+
+
+
+
