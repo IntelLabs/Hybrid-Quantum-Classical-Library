658f06c2cb6e1e08865e2336eeb9e436569df700
Atul Kulkarni <atul.kulkarni@intel.com>
2022-03-01 14:02:47 -0800

    examples: added angles as constansts to make the example work and minor whitespace removal

--- examples/tfd_q4_fullTempOptimization.cpp
+++ examples/tfd_q4_fullTempOptimization.cpp
@@ -35,14 +35,14 @@
 // Experiment:
 //
 // 1. Sagastizabal, R. et al. Variational preparation of  finite-temperature
-// states on a quantum computer (2021). npj Quantum Inf. 7:130 (2021) 
+// states on a quantum computer (2021). npj Quantum Inf. 7:130 (2021)
 // https://doi.org/10.1038/s41534-021-00468-1.
 //
 // 2. Zhu, D. et al. Generation of thermofield double states and critical ground
 // states with a quantum computer. P. Natl Acad. Sci. USA 117, 25402{25406
 // (2020). https://doi.org/10.1073/pnas.2006337117
 //
-// 3. Premaratne, S. et al. Engineering a Cost Function for Real-world 
+// 3. Premaratne, S. et al. Engineering a Cost Function for Real-world
 // Implementation of a Variational Quantum Algorithm (2020).
 // https://doi.org/10.1109/QCE49297.2020.00042.
 //===----------------------------------------------------------------------===//
@@ -54,16 +54,17 @@
 // #include <clang/Quantum/quintrinsics.h>
 
 // AUTHOR : Shavindra Premaratne (August 26th 2021)
-// Update : Comments added throughout to clarify the intentions behind code blocks (08/27/2021)
+// Update : Comments added throughout to clarify the intentions behind code
+// blocks (08/27/2021)
 
 /// Development mode
 #include "../../clang/include/clang/Quantum/quintrinsics.h"
 
-#include <iostream>
-#include <vector> 
-#include <fstream> // to write to files
 #include <cassert> // to assert the size of the Probability Register
+#include <fstream> // to write to files
+#include <iostream>
 #include <math.h> // to use pow()
+#include <vector>
 
 #include <qrt0.hpp>
 
@@ -81,30 +82,31 @@ const int N = 4;
 qbit QubitReg[N];
 cbit CReg[N];
 
-/* Special global vector from QRT to get state probabilities */
+// Special global vector from QRT to get state probabilities
 extern std::vector<double> ProbabilityRegister;
 
-/* Special global array to hold dynamic parameters for quantum algorithm */
-/* alpha1, alpha2, gamma1, gamma2 is the order of the variational angles used in here */
+// Special global array to hold dynamic parameters for quantum algorithm
+// alpha1, alpha2, gamma1, gamma2 is the order of the variational angles used in
+// here
 quantum_shared_double_array QuantumVariableParams[N];
 
 static int steps_count = 0;
 
-/* 
-When using the Intel Quantum Simulator (IQS) at the lowest level, it is necessary to 
-decide how to calculate the cost function based on the results returned. If using the full state
-vector, then only one experiment is required per iteration. 
+// When using the Intel Quantum Simulator (IQS) at the lowest level, it is
+// necessary to decide how to calculate the cost function based on the results
+// returned. If using the full state vector, then only one experiment is
+// required per iteration.
+//
+// In this script, we will be using the probabilities corresponding to each of
+// the basis states (e.g. 0000, 1010) These are returned simply by calculating
+// the probability corresponding to each of the state vector element and stored
+// in the special array ProbabilityRegister.
 
-In this script, we will be using the probabilities corresponding to each of the basis states (e.g. 0000, 1010)
-These are returned simply by calculating the probability corresponding to each of the state vector element
-and stored in the special array ProbabilityRegister.
-*/
+// First kind of experiment that needs to be run per iteration of optimization.
+// Contains the basic quantum operations as described in Fig. 1 of Ref. 1
+// with the appropriate decompositions such that the Intel Quantum Compiler can
+// handle them.
 
-/* 
-First kind of experiment that needs to be run per iteration of optimization. 
-Contains the basic quantum operations as described in Fig. 1 of Ref. 1 
-with the appropriate decompositions such that the Intel Quantum Compiler can handle them.
-*/
 quantum_kernel void tfdQ4_Z() {
   // Index to loop over later
   int Index = 0;
@@ -156,12 +158,12 @@ quantum_kernel void tfdQ4_Z() {
     MeasZ(QubitReg[Index], CReg[Index]);
 }
 
-/*
-Second kind of experiment that needs to be run per iteration of optimizaiton.
-Almost identical to the previous quantum kernel tfdQ4_Z, in this qkernel, four Hadamard operations are inserted
-just prior to measurement of the qubits. This enables mapping from X-basis to Z-basis, so that X related 
-observables can be calculated.
-*/
+// Second kind of experiment that needs to be run per iteration of optimizaiton.
+// Almost identical to the previous quantum kernel tfdQ4_Z, in this qkernel,
+// four Hadamard operations are inserted just prior to measurement of the
+// qubits. This enables mapping from X-basis to Z-basis, so that X related
+// observables can be calculated.
+
 quantum_kernel void tfdQ4_X() {
   // Index to loop over later
   int Index = 0;
@@ -207,7 +209,7 @@ quantum_kernel void tfdQ4_X() {
     RZ(QubitReg[Index + 2], QuantumVariableParams[1]);
   for (Index = 0; Index < 2; Index++)
     CNOT(QubitReg[Index], QubitReg[Index + 2]);
-    
+
   // Mapping from X basis to Z basis
   for (Index = 0; Index < N; Index++)
     H(QubitReg[Index]);
@@ -217,33 +219,37 @@ quantum_kernel void tfdQ4_X() {
     MeasZ(QubitReg[Index], CReg[Index]);
 }
 
-/* 
-The three expectation values of the observables have been written as functions below. The relationships between
-the expectation values and the probabilities obtained from the state vector amplitudes was derived independently 
-with simple matrix calculations of Pauli matrix products.
-*/
-double ZZApZZB_expectation (std::vector<double> ProbReg) {
-  auto line0 = ProbReg[0] + ProbReg[3] - ProbReg[5] - ProbReg[6] - ProbReg[9] - ProbReg[10] + ProbReg[12] + ProbReg[15];
+// The three expectation values of the observables have been written as
+// functions below. The relationships between the expectation values and the
+// probabilities obtained from the state vector amplitudes was derived
+// independently with simple matrix calculations of Pauli matrix products.
+
+double ZZApZZB_expectation(std::vector<double> ProbReg) {
+  auto line0 = ProbReg[0] + ProbReg[3] - ProbReg[5] - ProbReg[6] - ProbReg[9] -
+               ProbReg[10] + ProbReg[12] + ProbReg[15];
   return 2 * line0;
 }
 
-double ZZAB_expectation (std::vector<double> ProbReg) {
-  auto line0 = ProbReg[0] - ProbReg[3] + ProbReg[5] - ProbReg[6] - ProbReg[9] + ProbReg[10] - ProbReg[12] + ProbReg[15];
+double ZZAB_expectation(std::vector<double> ProbReg) {
+  auto line0 = ProbReg[0] - ProbReg[3] + ProbReg[5] - ProbReg[6] - ProbReg[9] +
+               ProbReg[10] - ProbReg[12] + ProbReg[15];
   return 2 * line0;
 }
 
-double ZApZB_expectation (std::vector<double> ProbReg) {
+double ZApZB_expectation(std::vector<double> ProbReg) {
   auto line0 = ProbReg[0] - ProbReg[15];
-  auto line1 = ProbReg[1] + ProbReg[2] + ProbReg[4] - ProbReg[7] + ProbReg[8] - ProbReg[11] - ProbReg[13] - ProbReg[14];
+  auto line1 = ProbReg[1] + ProbReg[2] + ProbReg[4] - ProbReg[7] + ProbReg[8] -
+               ProbReg[11] - ProbReg[13] - ProbReg[14];
   return (4 * line0 + 2 * line1);
 }
 
-/*
-The total cost function is constructed here using the expectation values from before. There is freedom in the numeric
-coefficient and index used. Here we use the best engineered simple cost function from Ref. 3
-*/
-double total_cost (double inv_temp, std::vector<double> ProbReg_Z, std::vector<double> ProbReg_X) {
-  double energy_X = ZApZB_expectation(ProbReg_X); 
+// The total cost function is constructed here using the expectation values from
+// before. There is freedom in the numeric coefficient and index used. Here we
+// use the best engineered simple cost function from Ref. 3
+
+double total_cost(double inv_temp, std::vector<double> ProbReg_Z,
+                  std::vector<double> ProbReg_X) {
+  double energy_X = ZApZB_expectation(ProbReg_X);
   double energy_ZZ = ZZApZZB_expectation(ProbReg_Z);
   double entropy = ZZAB_expectation(ProbReg_X) + ZZAB_expectation(ProbReg_Z);
   return energy_X + 1.60 * energy_ZZ - pow(inv_temp, -1.48) * entropy;
@@ -251,48 +257,52 @@ double total_cost (double inv_temp, std::vector<double> ProbReg_Z, std::vector<d
 
 // Constructing a lambda function to be used for a single optimization iteration
 // This function is directly called by the dlib optimization routine
-double ansatz_run_lambda (const arma::mat &m, double inv_temp) {
-    // loading the new variational angles into the special global array for hybrid compilation
-    QuantumVariableParams[0] = m(0); 
-    QuantumVariableParams[1] = m(1);
-    QuantumVariableParams[2] = m(2);
-    QuantumVariableParams[3] = m(3);
-    
-    // two local variables to store the results from the two different experiments run in each iteration
-    std::vector<double> ProbRegZ;
-    std::vector<double> ProbRegX;
-    
-    // performing the Z-focused experiment, and storing the data in ProbRegZ
-    tfdQ4_Z();
-    std::cout << "Z - Probability Register Size: " << ProbabilityRegister.size() << "\n";
-    for (auto j = 0; j < ProbabilityRegister.size(); j++) {
-      auto p = ProbabilityRegister[j];
-      if (p > 0) {
-        ProbRegZ.push_back(ProbabilityRegister[j]);
-        std::cout << j << " : " << ProbabilityRegister[j] << "\t";
-        if ((j + 1) % 8 == 0)
-          std::cout << "\n";
-      }
+double ansatz_run_lambda(const arma::mat &m, double inv_temp) {
+  // loading the new variational angles into the special global array for hybrid
+  // compilation
+  QuantumVariableParams[0] = m(0);
+  QuantumVariableParams[1] = m(1);
+  QuantumVariableParams[2] = m(2);
+  QuantumVariableParams[3] = m(3);
+
+  // two local variables to store the results from the two different experiments
+  // run in each iteration
+  std::vector<double> ProbRegZ;
+  std::vector<double> ProbRegX;
+
+  // performing the Z-focused experiment, and storing the data in ProbRegZ
+  tfdQ4_Z();
+  std::cout << "Z - Probability Register Size: " << ProbabilityRegister.size()
+            << "\n";
+  for (auto j = 0; j < ProbabilityRegister.size(); j++) {
+    auto p = ProbabilityRegister[j];
+    if (p > 0) {
+      ProbRegZ.push_back(ProbabilityRegister[j]);
+      std::cout << j << " : " << ProbabilityRegister[j] << "\t";
+      if ((j + 1) % 8 == 0)
+        std::cout << "\n";
     }
-    std::cout << "\n";
-    
-    // performing the X-focused experiment, and storing the data in ProbRegZ
-    tfdQ4_X();
-    std::cout << "X - Probability Register Size: " << ProbabilityRegister.size() << "\n";
-    for (auto j = 0; j < ProbabilityRegister.size(); j++) {
-      auto p = ProbabilityRegister[j];
-      if (p > 0) {
-        ProbRegX.push_back(ProbabilityRegister[j]);
-        std::cout << j << " : " << ProbabilityRegister[j] << "\t";
-        if ((j + 1) % 8 == 0)
-          std::cout << "\n";
-      }
+  }
+  std::cout << "\n";
+
+  // performing the X-focused experiment, and storing the data in ProbRegZ
+  tfdQ4_X();
+  std::cout << "X - Probability Register Size: " << ProbabilityRegister.size()
+            << "\n";
+  for (auto j = 0; j < ProbabilityRegister.size(); j++) {
+    auto p = ProbabilityRegister[j];
+    if (p > 0) {
+      ProbRegX.push_back(ProbabilityRegister[j]);
+      std::cout << j << " : " << ProbabilityRegister[j] << "\t";
+      if ((j + 1) % 8 == 0)
+        std::cout << "\n";
     }
-    std::cout << "\n";
-    
-    // calculating the cost function for this iteration
-    double calc_cost = total_cost(inv_temp, ProbRegZ, ProbRegX);        
-    return calc_cost;
+  }
+  std::cout << "\n";
+
+  // calculating the cost function for this iteration
+  double calc_cost = total_cost(inv_temp, ProbRegZ, ProbRegX);
+  return calc_cost;
 }
 
 class EnergyOfAnsatz {
@@ -318,63 +328,67 @@ private:
   double beta;
 };
 
-int main()
-{
-    // resetting steps count
-    steps_count = 0;
+int main() {
+  // resetting steps count
+  steps_count = 0;
 
-    // resetting steps count
-    steps_count = 0;
+  // resetting steps count
+  steps_count = 0;
 
-    // Clearing the file contents used to store all the results from TFD minimization
-    std::ofstream file_to_clear;
-    file_to_clear.open("tfd_minimization_results.csv", std::ofstream::out | std::ofstream::trunc);
-    file_to_clear.close();
+  // Clearing the file contents used to store all the results from TFD
+  // minimization
+  std::ofstream file_to_clear;
+  file_to_clear.open("tfd_minimization_results.csv",
+                     std::ofstream::out | std::ofstream::trunc);
+  file_to_clear.close();
 
-    std::ofstream results_file;
-    results_file.open("tfd_minimization_results.csv", std::ios_base::app);
-    for (int beta_index = -30; beta_index < 31; beta_index+=1) {
-      // calculating the actual inverse temperature that is used during calculations
-      double beta = pow(10, (double) beta_index/10);
+  std::ofstream results_file;
+  results_file.open("tfd_minimization_results.csv", std::ios_base::app);
+  for (int beta_index = -30; beta_index < 31; beta_index += 1) {
+    // calculating the actual inverse temperature that is used during
+    // calculations
+    double beta = pow(10, (double)beta_index / 10);
 
-      // Using SA algorithm
-      // Starting parameters
-      arma::mat params_sa(std::vector<double>{0, 0, 0, 0});
+    // Using SA algorithm
+    // Starting parameters
+    arma::mat params_sa(std::vector<double>{0, 0, 0, 0});
 
-      // arbitrary function
-      EnergyOfAnsatz eoa_sa(params_sa, beta);
+    // arbitrary function
+    EnergyOfAnsatz eoa_sa(params_sa, beta);
 
-      // initialize the optimization algorithm
-      ens::SA<> opt_sa(
-          ens::ExponentialSchedule(), /* coolingSchedule - Instantiated cooling
-                                         schedule (default ExponentialSchedule). */
-          1000000, /* maxIterations - Maximum number of iterations allowed (0
-                      indicates no limit). */
-          1000.,   /* initT - Initial temperature. */
-          1000,    /* initMoves - Number of initial iterations without changing
-                      temperature. */
-          100,     /* moveCtrlSweep - Sweeps per feedback move control. */
-          1e-7,    /* tolerance - Tolerance to consider system frozen. */
-          3,       /* maxToleranceSweep - Maximum sweeps below tolerance to consider
-                      system frozen. */
-          1.5,     /* maxMoveCoef - Maximum move size. */
-          0.5,     /* initMoveCoef - Initial move size. */
-          0.3      /* gain - Proportional control in feedback move control. */
-      );
+    // initialize the optimization algorithm
+    ens::SA<> opt_sa(
+        ens::ExponentialSchedule(), /* coolingSchedule - Instantiated cooling
+                                       schedule (default ExponentialSchedule).
+                                     */
+        1000000, /* maxIterations - Maximum number of iterations allowed (0
+                    indicates no limit). */
+        1000.,   /* initT - Initial temperature. */
+        1000,    /* initMoves - Number of initial iterations without changing
+                    temperature. */
+        100,     /* moveCtrlSweep - Sweeps per feedback move control. */
+        1e-7,    /* tolerance - Tolerance to consider system frozen. */
+        3,   /* maxToleranceSweep - Maximum sweeps below tolerance to consider
+                system frozen. */
+        1.5, /* maxMoveCoef - Maximum move size. */
+        0.5, /* initMoveCoef - Initial move size. */
+        0.3  /* gain - Proportional control in feedback move control. */
+    );
 
-      // optimize the parameters
-      double opt_sa_val = opt_sa.Optimize(eoa_sa, params_sa);
-      int sa_steps_count = steps_count;
-      std::cout << "SA Step Count: " << sa_steps_count << "\n";
-      
-      // writing the results from a single temperature to file
-      results_file << beta << ", ";
-      for (int i = 0; i < 4; i++)
-        results_file << params_sa[i] << ", ";
-      results_file << opt_sa_val << "\n";
-    }
-    
-    results_file.close();
+    // optimize the parameters
+    double opt_sa_val = opt_sa.Optimize(eoa_sa, params_sa);
+    int sa_steps_count = steps_count;
+    std::cout << "SA Step Count: " << sa_steps_count << "\n";
 
-    return 0;
+    // writing the results from a single temperature to file
+    results_file << beta << ", ";
+    for (int i = 0; i < 4; i++)
+      results_file << params_sa[i] << ", ";
+    results_file << opt_sa_val << "\n";
+  }
+
+  results_file.close();
+
+  return 0;
 }
+
