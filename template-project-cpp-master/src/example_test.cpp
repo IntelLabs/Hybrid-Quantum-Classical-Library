e0db753d55e3642113ad7cca4b512abca746b202
Atul Kulkarni <atul.kulkarni@intel.com>
2022-03-29 18:21:27 -0700

    maint: removed files and directories not required by the project

--- template-project-cpp-master/src/example_test.cpp
+++
@@ -1,8 +0,0 @@
-#include "../third_party/googletest/googletest/include/gtest/gtest.h"
-
-#include "example.hpp"
-
-TEST (AdditionTest, Complete) { 
-    EXPECT_EQ(4, addition(2, 2));
-    EXPECT_EQ(12, addition(-2, 14));
-}
