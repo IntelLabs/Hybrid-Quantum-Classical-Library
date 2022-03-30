7e22361178712d3f03e6df8c4e6b33661da76334
Atul Kulkarni <atul.kulkarni@intel.com>
2022-03-30 08:57:59 -0700

    maint: Updated copyright headers in the source files

--- examples/vqe_q2.cpp
+++ examples/vqe_q2.cpp
@@ -1,7 +1,7 @@
 //===----------------------------------------------------------------------===//
 // INTEL CONFIDENTIAL
 //
-// Copyright 2021 Intel Corporation.
+// Copyright 2021-2022 Intel Corporation.
 //
 // This software and the related documents are Intel copyrighted materials, and
 // your use of them is governed by the express license under which they were
