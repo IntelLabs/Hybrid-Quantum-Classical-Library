846965949557441f3a2eded08a05aa51d057c20b
Atul Kulkarni <atul.kulkarni@intel.com>
2022-03-29 18:21:27 -0700

    maint: removed files and directories not required by the project

--- macros.cmake
+++
@@ -1,7 +0,0 @@
-
-macro(package_add_test_with_libraries TESTNAME FILES LIBRARIES TEST_WORKING_DIRECTORY)
-    add_executable(${TESTNAME} ${FILES})
-    target_link_libraries(${TESTNAME} gtest gmock gtest_main ${LIBRARIES})
-    set_target_properties(${TESTNAME} PROPERTIES FOLDER utests)
-endmacro()
-
